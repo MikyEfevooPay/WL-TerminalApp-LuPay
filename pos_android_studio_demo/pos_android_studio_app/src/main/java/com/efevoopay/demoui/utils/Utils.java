980708a05d6f682e83890b6b5dc21036ae68ae16
Rooseveltdelagarza <110432712+Rooseveltdelagarza@users.noreply.github.com>
2023-10-13 07:46:06 -0600

    Apunta las URL a producción

--- pos_android_studio_demo/pos_android_studio_app/src/main/java/com/efevoopay/demoui/utils/Utils.java
+++ pos_android_studio_demo/pos_android_studio_app/src/main/java/com/efevoopay/demoui/utils/Utils.java
@@ -21,9 +21,9 @@ import java.util.regex.Pattern;
 
 public class Utils {
 
-	public static final String TERMINAL_API = "https://test-efevoopayloadbalancer-ecommerce.com"; //"https://efevoopayloadbalancer-ecommerce.com";
+	public static final String TERMINAL_API = "https://efevoopayloadbalancer-ecommerce.com";
 	public static final String TERMINAL_API_TEST = "https://test-efevoopayloadbalancer-ecommerce.com";
-	public static final String TERMINAL_BIN = "https://test-agrs.gntapi.com/apiv0/agrs/terminales/tpv/bines";//"https://testagrswl.gntapi.com/apiv0/agrs/terminales/tpv/bines";
+	public static final String TERMINAL_BIN = "https://testagrswl.gntapi.com/apiv0/agrs/terminales/tpv/bines";
 	public static final String TERMINAL_BIN_TEST = "https://test-agrs.gntapi.com/apiv0/agrs/terminales/tpv/bines";
 	public static final String TPVCONFIG = "https://testagrswl.gntapi.com";
 	public static HashMap<String,String> errorMessagesDictionary;
