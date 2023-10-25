dac3e87adbab0fe9dac4f04ea179731b0526c071
IlanArellano22 <110840319+IlanArellano22@users.noreply.github.com>
2023-10-25 08:51:22 -0600

    Update Ticket.java

--- pos_android_studio_demo/pos_android_studio_app/src/main/java/com/efevoopay/demoui/utils/Ticket.java
+++ pos_android_studio_demo/pos_android_studio_app/src/main/java/com/efevoopay/demoui/utils/Ticket.java
@@ -1,7 +1,13 @@
 package com.efevoopay.demoui.utils;
 
+import android.content.ContentResolver;
+import android.database.CharArrayBuffer;
+import android.database.ContentObserver;
 import android.database.Cursor;
+import android.database.DataSetObserver;
+import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.RemoteException;
 import android.view.View;
 
@@ -9,6 +15,7 @@ import com.action.printerservice.ActionPrinter;
 import com.dspread.print.device.PrintListener;
 import com.dspread.print.device.PrinterDevice;
 import com.dspread.print.device.PrinterManager;
+import com.dspread.xpos.Util;
 
 import java.util.Locale;
 
@@ -55,74 +62,74 @@ public class Ticket {
 
     public int getTrans_id() { return this.trans_id; }
     public String getTrans_type() {
-        return this.trans_type.toUpperCase(Locale.ROOT);
+        return Utils.isNull(this.trans_type, "").toUpperCase(Locale.ROOT);
     }
 
     public String getApprove() {
-        return this.approve;
+        return Utils.isNull(this.approve, "");
     }
 
     public String getCard() {
-        return this.card;
+        return Utils.isNull(this.card, "");
     }
 
     public String getCard_type() {
-        return this.card_type;
+        return Utils.isNull(this.card_type, "");
     }
 
     public String getDate_time() {
-        return this.date_time;
+        return Utils.isNull(this.date_time, "");
     }
 
     public String getAmount() {
-        return this.amount;
+        return Utils.isNull(this.amount, "");
     }
 
     public String getTip() {
-        return this.tip;
+        return Utils.isNull(this.tip, "");
     }
 
     public String getTotal() {
-        return this.total;
+        return Utils.isNull(this.total, "");
     }
 
     public String getARQC() {
-        return this.ARQC;
+        return Utils.isNull(this.ARQC, "");
     }
 
     public String getAID() {
-        return this.AID;
+        return Utils.isNull(this.AID, "");
     }
 
     public String getKsn_posId() {
-        return this.ksn_posId;
+        return Utils.isNull(this.ksn_posId, "");
     }
 
     public String getCard_provider() {
-        return card_provider;
+        return Utils.isNull(this.card_provider, "");
     }
 
     public String getStatus() {
-        return status;
+        return Utils.isNull(this.status, "");
     }
 
     public Cursor getCursor() {
-        return this.cursor;
+        return Utils.isNull(this.cursor, Ticket.createDefaultCursor());
     }
 
     public String getMsi() {
-        return this.msi;
+        return Utils.isNull(this.msi, "");
     }
 
     public String getCard_emisor() {
-        return card_emisor;
+        return Utils.isNull(this.card_emisor, "");
     }
 
-    public String getCard_nip() {return card_nip; }
+    public String getCard_nip() {return Utils.isNull(this.card_nip, ""); }
 
-    public String getCard_entrada() {return card_entrada; }
+    public String getCard_entrada() {return Utils.isNull(this.card_entrada, ""); }
 
-    public String getCard_singtype() {return card_singtype; }
+    public String getCard_singtype() {return Utils.isNull(this.card_singtype, ""); }
 
     // Setters
     public Ticket setTransId(int trans_id) {
@@ -250,6 +257,215 @@ public class Ticket {
 
     public boolean isPrinterAvailable() {
         return printeravailable && Build.MODEL.equals("D30");
+    }
+
+    private static Cursor createDefaultCursor() {
+        return new Cursor() {
+            @Override
+            public int getCount() {
+                return 0;
+            }
+
+            @Override
+            public int getPosition() {
+                return 0;
+            }
+
+            @Override
+            public boolean move(int offset) {
+                return false;
+            }
+
+            @Override
+            public boolean moveToPosition(int position) {
+                return false;
+            }
+
+            @Override
+            public boolean moveToFirst() {
+                return false;
+            }
+
+            @Override
+            public boolean moveToLast() {
+                return false;
+            }
+
+            @Override
+            public boolean moveToNext() {
+                return false;
+            }
+
+            @Override
+            public boolean moveToPrevious() {
+                return false;
+            }
+
+            @Override
+            public boolean isFirst() {
+                return false;
+            }
+
+            @Override
+            public boolean isLast() {
+                return false;
+            }
+
+            @Override
+            public boolean isBeforeFirst() {
+                return false;
+            }
+
+            @Override
+            public boolean isAfterLast() {
+                return false;
+            }
+
+            @Override
+            public int getColumnIndex(String columnName) {
+                return 0;
+            }
+
+            @Override
+            public int getColumnIndexOrThrow(String columnName) throws IllegalArgumentException {
+                return 0;
+            }
+
+            @Override
+            public String getColumnName(int columnIndex) {
+                return null;
+            }
+
+            @Override
+            public String[] getColumnNames() {
+                return new String[0];
+            }
+
+            @Override
+            public int getColumnCount() {
+                return 0;
+            }
+
+            @Override
+            public byte[] getBlob(int columnIndex) {
+                return new byte[0];
+            }
+
+            @Override
+            public String getString(int columnIndex) {
+                return null;
+            }
+
+            @Override
+            public void copyStringToBuffer(int columnIndex, CharArrayBuffer buffer) {
+
+            }
+
+            @Override
+            public short getShort(int columnIndex) {
+                return 0;
+            }
+
+            @Override
+            public int getInt(int columnIndex) {
+                return 0;
+            }
+
+            @Override
+            public long getLong(int columnIndex) {
+                return 0;
+            }
+
+            @Override
+            public float getFloat(int columnIndex) {
+                return 0;
+            }
+
+            @Override
+            public double getDouble(int columnIndex) {
+                return 0;
+            }
+
+            @Override
+            public int getType(int columnIndex) {
+                return 0;
+            }
+
+            @Override
+            public boolean isNull(int columnIndex) {
+                return false;
+            }
+
+            @Override
+            public void deactivate() {
+
+            }
+
+            @Override
+            public boolean requery() {
+                return false;
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public boolean isClosed() {
+                return false;
+            }
+
+            @Override
+            public void registerContentObserver(ContentObserver observer) {
+
+            }
+
+            @Override
+            public void unregisterContentObserver(ContentObserver observer) {
+
+            }
+
+            @Override
+            public void registerDataSetObserver(DataSetObserver observer) {
+
+            }
+
+            @Override
+            public void unregisterDataSetObserver(DataSetObserver observer) {
+
+            }
+
+            @Override
+            public void setNotificationUri(ContentResolver cr, Uri uri) {
+
+            }
+
+            @Override
+            public Uri getNotificationUri() {
+                return null;
+            }
+
+            @Override
+            public boolean getWantsAllOnMoveCalls() {
+                return false;
+            }
+
+            @Override
+            public void setExtras(Bundle extras) {
+
+            }
+
+            @Override
+            public Bundle getExtras() {
+                return null;
+            }
+
+            @Override
+            public Bundle respond(Bundle extras) {
+                return null;
+            }
+        };
     }
 
 }
