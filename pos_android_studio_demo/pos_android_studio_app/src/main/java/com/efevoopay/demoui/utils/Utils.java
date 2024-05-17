0489a6660eb87cb5e4376d79dac9a11a4f6f785b
MickyNat <104525552+MickyNat@users.noreply.github.com>
2024-05-17 12:18:44 -0600

    version-1.5.9.4

--- pos_android_studio_demo/pos_android_studio_app/src/main/java/com/efevoopay/demoui/utils/Utils.java
+++ pos_android_studio_demo/pos_android_studio_app/src/main/java/com/efevoopay/demoui/utils/Utils.java
@@ -23,10 +23,10 @@ public class Utils {
 
 	public static final String TERMINAL_API = "https://efevoopayloadbalancer-ecommerce.com";
 	//public static final String TERMINAL_API = "https://test-efevoopayloadbalancer-ecommerce.com";
-	//public static final String TERMINAL_BIN = "https://testagrswl.efevoo.app/apiv0/agrs/terminales/tpv/bines";
-	public static final String TERMINAL_BIN = "https://test-agrs.efevoo.app/apiv0/agrs/terminales/tpv/bines";
-	//public static final String TPVCONFIG = "https://testagrswl.efevoo.app";
-	public static final String TPVCONFIG = "https://test-agrs.efevoo.app";
+	public static final String TERMINAL_BIN = "https://testagrswl.efevoo.app/apiv0/agrs/terminales/tpv/bines";
+	//public static final String TERMINAL_BIN = "https://test-agrs.efevoo.app/apiv0/agrs/terminales/tpv/bines";
+	public static final String TPVCONFIG = "https://testagrswl.efevoo.app";
+	//public static final String TPVCONFIG = "https://test-agrs.efevoo.app";
 	
 
 	public static HashMap<String,String> errorMessagesDictionary;
