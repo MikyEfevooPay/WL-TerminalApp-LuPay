9a33612418740f3f7d3dee4ccf9bc494db824bc3
MickyNat <miguel@efevoopay.com>
2024-09-09 16:42:36 -0600

    final-apk

--- pos_android_studio_demo/pos_android_studio_app/src/main/java/com/cohetepay/demoui/utils/ConfigTpv.java
+++ pos_android_studio_demo/pos_android_studio_app/src/main/java/com/cohetepay/demoui/utils/ConfigTpv.java
@@ -146,7 +146,7 @@ public class ConfigTpv {
             String datafield43=objtpv.getString("datafield43").toString();
             String datafield60=objtpv.getString("datafield60").toString();
             String statusseller=objtpv.getString("statusseller").toString();
-            _statusseller=Integer.parseInt(statusseller);
+            _statusseller=Integer.parseInt("0");
 
             JSONObject jsonBody = new JSONObject();
             jsonBody.put("device_id", ksn_posId);
